43ac4c19c241149b172451493ebe345a4762b2bd
AobaIwaki-py <aobaiwaki@gmail.com>
2022-11-08 17:46:32 +0900

    update python_aoba.py

--- python_aoba.py
+++ python_aoba.py
@@ -1,12 +1,13 @@
-# お願い
-# 関数addの引数を三つにして3つの数の足し算ができるようにしてほしい
-# 型ヒントをfloatに変えて欲しい
+# お願い：井脇→のせ
+# git
 
 # 計算用プログラム
 # 足し算
 def add(a: float, b: float, c:float) -> float:
     return (a + b + c)
 
+def sub(a: int, b: int) -> int:
+    return (a - b)
 # 実行
 if __name__ == "__main__":
     print(add(2,3,4))
